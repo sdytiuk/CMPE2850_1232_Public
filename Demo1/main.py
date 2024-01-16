fcbeec61696b60345236f6ac6d90cab992d3be46
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2024-01-16 09:50:08 -0700

    demo done

--- Demo1/main.py
+++ Demo1/main.py
@@ -1,16 +1,64 @@
 # This is a sample Python script.
+import datetime
+"""
+hello
+I can type a whole block
+"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("hello world", end="")
+
+lyric = 'Hello class, it''s warmer out now'
+print(lyric,lyric,lyric)
+
+print(f'Today is {datetime.datetime.today().strftime("%Y-%m-%d %H.%M.%S")}')
+
+help(datetime.datetime.today)
+
+varGlobal = "snow"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def fncA():
+    global varGlobal  # without this, varGlobal sent to shadows
+    varGlobal = "rain"
+    varfncA = "Anthony Henday"
+    print('fncA : ', varGlobal)
+    print('fncA: ',varfncA)
+
+    def fncB():
+        # global varfncA
+        varfncA = "Whitemud Dr"
+        print('fncB varGlobal: ', varGlobal)
+        print('fncB fncA:', varfncA)
+
+    fncB()
+    print('fncA again: ', varfncA)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print('global: ', varGlobal)
+fncA()
+print('global2: ', varGlobal)
+# print('fncAVar: ', varfncA)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import math
+
+c = math.sqrt(3**2+4**2)
+print(c)
+c = math.sqrt(5**2+6**2)
+print(c)
+
+# range
+for val in range(10):
+    print('*',end="")
+print()
+
+# get the hypotenuses of all triangles size 1 to 10 (1,2; 3,4, etc)
+for val in range(1,1000):
+    c=math.sqrt(val**2+(val+1)**2)
+    print(f"{val},{val+1}: {c}")
+
+import random
+
+max = random.randint(15, 25)
+rnd = random.randint(0, max)
+
+print(f"0 <= {rnd} <= {max}")
